0508234f8357c3fd963d55d92755f7f7f1d48b39
Ranganath K <ranganathkomandur@gmail.com>
2015-12-10 14:40:39 +0530

    rename methods in user.rb and access remember_token in tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,32 @@ class User < ActiveRecord::Base
   has_secure_password
   validates(:password, presence: true, length: { minimum: 6 }) 
 
-  # Returns the hash digest of the given string
-  def User.digest(string)
+  def self.digest(string) # User.method can also be used
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  def self.new_token # User.method can also be used
     SecureRandom.urlsafe_base64
   end
+
+  # The methods below can be used instead of those above and carry out the same
+  # operations. Here class inherits from the self class and can use methods defined
+  # in self class. 
+
+  # class << self
+  # # Returns the hash digest of the given string
+  #   def digest(string)
+  #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+  #     BCrypt::Password.create(string, cost: cost)
+  #   end
+
+  #   # Returns a random token.
+  #   def new_token
+  #     SecureRandom.urlsafe_base64
+  #   end
+  # end
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
