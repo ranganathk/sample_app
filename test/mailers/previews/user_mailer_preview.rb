e0d8b7a1ff82db78181444e4ff68be46a2d92829
Ranganath K <ranganathkomandur@gmail.com>
2015-12-16 12:58:31 +0530

    Add account activations

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,17 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first
+    # User from database does not have an activation token so we create one for him.
+    user.activation_token = User.new_token  
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+
+end
